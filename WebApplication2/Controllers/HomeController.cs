28a3830615345f86e166827e214e6c93b5c4cae4
Pochelyk <Pochelyk@mail.ru>
2017-04-17 15:02:28 +0300

    Запросы Get b Post Основы контроллеров методы контроллеров и их параметры

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult GetBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string PostBook()
+        {
+            string title = Request.Form["title"];
+            string autor = Request.Form["autor"];
+            return title + " " + autor;
+        }
+        //public string Square(int a, int h)
+        public string Square()
+        {   int a = Int32.Parse(Request.Params[ "a"]);
+            int h = Int32.Parse(Request.Params[ "h"]);
+            double s = a * h / 2;
+            return "<h2>Площадь треугольника с основанием " + a +
+                    " и высотой " + h + " равна " + s + "</h2>";
+        }
+        public string GetId(int id)
+        {
+            return id.ToString();
+        }
+
+        private ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
